8fabe2b0a1f07113251ad9992ed931513699391a
lajosandris <lakanalas@gmail.com>
2023-02-16 18:40:07 +0100

    asd

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 
-var tablica = "<table border=1> </table> "
+var tablica = "<table border=1>\n";
 
+for( var i=1; i<=10; i++){
+    console.log("iteracija: " + i)
+
+    tablica +="<tr>\n" +
+     "<td>Name" + i +" </td>\n" +
+     "<td>Rank " + i +"</td>\n" +
+     "<td>Class " + i +"</td>\n" +
+     "<tr>\n"
+
+}
+
+tablica += "</table>";
+console.log(tablica);
 document.write(tablica);
-
-
-console.log(tablica)
-
---
+++ tipovi_podataka.js
@@ -0,0 +1,10 @@
+let x = "Lorem ipsum",
+    y= 2345,
+    z="2345"
+    q= false,
+    w= null;
+
+
+    let baaa = "b" + "a" + + "a" + "a";
+
+    document.write(baaa)
