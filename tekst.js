f46431555923250cf7261bc2596134abfc1acbb4
lajosandris <lakanalas@gmail.com>
2023-02-16 18:57:50 +0100

    asd

---
+++ tekst.js
@@ -0,0 +1,16 @@
+let text = " Lorem ipsum dolor sit  amet lalalala";
+
+let length = text.length;
+
+//console.log("duljina teksta je=" + length);
+
+
+let wordSit = text.substr(19,3);
+let wordSit2 = text.substring(19, 19 + 3);
+
+
+let text2= text.replace("amet", "elit");
+console.log(text2);
+
+return;
+
