accb440f96f12d77404d575d1ff76007193d02eb
lajosandris <lakanalas@gmail.com>
2023-02-24 17:50:42 +0100

    asd

--- vjezba_24.2.js
+++ vjezba_24.2.js
@@ -8,12 +8,38 @@ let auto = {
     minBrzina: 0,
     trenutnaBrzina: 0,
     ubrzaj: function(ubrzanje) {
-        let brzina=0;
+      
         this.trenutnaBrzina+=ubrzanje;
         return this.trenutnaBrzina;
     },
-    //zakoci: function(),
+    zakoci: function(brojSekundi){
+        while(this.trenutnaBrzina > 0 && brojSekundi >=0){
+            this.trenutnaBrzina -=2;
+            console.info("Trenutna brzina pri kocenju je: " + this.trenutnaBrzina);
+            brojSekundi--;
+        }
+
+        return this.trenutnaBrzina;
+    },
 }
 
 
+for(let key in auto){
+    if(auto.propertyIsEnumerable(key)){
+        console.log("kljuc: " + key + "vrijesdnost: " + auto[key] );
+    }
+}
+
+
+
 console.log("Ja vozim auto marke:" +auto.marka);
+
+console.log("Auto vozi brzinom" + auto.trenutnaBrzina);
+
+console.log("auto vozi nakon ubrzanja brzinom " + auto.ubrzaj(10));
+
+console.log("auto vozi nakon kocenja brzinom " + auto.zakoci(2));
+
+
+let json= JSON.stringify(auto);
+console.log(json);
